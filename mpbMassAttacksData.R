547bf2daaced52f8e4834d9ba2a3bf3f1c6b8fd3
Eliot McIntire <eliotmcintire@gmail.com>
2021-05-20 14:55:07 -0700

    deal with gdb datasets; loading all data;

--- mpbMassAttacksData.R
+++ mpbMassAttacksData.R
@@ -14,7 +14,7 @@ defineModule(sim, list(
   timeunit = "year",
   citation = list(),
   documentation = list(),
-  reqdPkgs = list("achubaty/amc@development", "data.table",
+  reqdPkgs = list("achubaty/amc@development", "archive", "data.table",
                   "PredictiveEcology/LandR@development",
                   "PredictiveEcology/reproducible@customFun (>= 1.2.6.9018)",
                   "magrittr", "quickPlot",
@@ -56,14 +56,11 @@ defineModule(sim, list(
   ),
   outputObjects = bindrows(
     createsOutput("currentAttacks", "RasterLayer",
-                  desc = "Current year MPB attack maps (number of red attacked trees).",
-                  sourceURL = NA),
+                  desc = "Current year MPB attack maps (number of red attacked trees)."),
     createsOutput("massAttacksMap", "RasterStack",
-                  desc = "Historical MPB attack maps (number of red attacked trees).",
-                  sourceURL = NA),
+                  desc = "Historical MPB attack maps (number of red attacked trees)."),
     createsOutput("massAttacksDT", "data.table",
-                  desc = "same data (though presence only) as currentAttacks, but in data.table format. Colnames: ID for pixelID and ATKTREES for number of attacked trees",
-                  sourceURL = NA)
+                  desc = "same data (though presence only) as currentAttacks, but in data.table format. Colnames: ID for pixelID and ATKTREES for number of attacked trees")
   )
 ))
 
@@ -72,28 +69,28 @@ defineModule(sim, list(
 
 doEvent.mpbMassAttacksData <- function(sim, eventTime, eventType, debug = FALSE) {
   switch(eventType,
-    "init" = {
-      ### check for more detailed object dependencies:
-      ### (use `checkObject` or similar)
+         "init" = {
+           ### check for more detailed object dependencies:
+           ### (use `checkObject` or similar)
 
-      # do stuff for this event
-      sim <- Init(sim)
+           # do stuff for this event
+           sim <- Init(sim)
 
-      # schedule future event(s)
-      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "mpbMassAttacksData", "plot", .last() - 1)
-      sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "mpbMassAttacksData", "save", .last())
-    },
-    "plot" = {
-      Plot(sim$currentAttacks)
-      Plot(sim$studyArea, addTo = "sim$currentAttacks", gp = gpar(col = "black", fill = 0),
-           title = "")
-      Plot(sim$studyAreaLarge, addTo = "sim$currentAttacks", gp = gpar(col = "black", fill = 0),
-           title = "")
+           # schedule future event(s)
+           sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "mpbMassAttacksData", "plot", .last() - 1)
+           sim <- scheduleEvent(sim, P(sim)$.saveInitialTime, "mpbMassAttacksData", "save", .last())
+         },
+         "plot" = {
+           Plot(sim$currentAttacks)
+           Plot(sim$studyArea, addTo = "sim$currentAttacks", gp = gpar(col = "black", fill = 0),
+                title = "")
+           Plot(sim$studyAreaLarge, addTo = "sim$currentAttacks", gp = gpar(col = "black", fill = 0),
+                title = "")
 
-      sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "mpbMassAttacksData", "plot", .last() - 1)
-    },
-    warning(paste("Undefined event type: '", events(sim)[1, "eventType", with = FALSE],
-                  "' in module '", events(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
+           sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "mpbMassAttacksData", "plot", .last() - 1)
+         },
+         warning(paste("Undefined event type: '", events(sim)[1, "eventType", with = FALSE],
+                       "' in module '", events(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
   )
   return(invisible(sim))
 }
@@ -145,22 +142,24 @@ doEvent.mpbMassAttacksData <- function(sim, eventTime, eventType, debug = FALSE)
 Init <- function(sim) {
   ## TODO: incorporate code from MPB_maps.R to create the raster layers
 
-  sim$massAttacksMap <- Cache(prepInputs,
-                              url = extractURL("massAttacksMapFile"),
-                              destinationPath = asPath(dataPath(sim)),
-                              # Custom function for loading to R, with 1 custom arg + function itself
-                              fun = quote(
-                                loadRasterStackTruncateYears(targetFilePath, startTime = startTime)
-                              ),
-                              loadRasterStackTruncateYears = loadRasterStackTruncateYears,
-                              startTime = start(sim),
-                              # For postProcessing
-                              rasterToMatch = sim$rasterToMatch,
-                              filename2 = NULL,
-                              method = "bilinear",
-                              datatype = "FLT4S",
-                              overwrite = TRUE,
-                              userTags = c("stable", currentModule(sim)))
+  datasets <- c(
+    "https://drive.google.com/file/d/11YoAxkHzWNsNGkNtoe5ph5BdT5xXUoGg/view?usp=sharing",  # 1970s to 2018
+    "https://drive.google.com/file/d/1vhSLJf03KTi0Oeec_pSiwK7EQZYd_PlF/view?usp=sharing", # 2019
+    "https://drive.google.com/file/d/1S5Lw5g5ACcTyhf8kwR7sqwPzGOCjfpwB/view?usp=sharing" # 2020
+  )
+  out <- lapply(datasets, function(url) {
+    prepInputsMPB_ABdata(url = url,
+                         startYear = start(sim),
+                         rasterToMatch = sim$rasterToMatch,
+                         disaggregateFactor = 10)
+  })
+
+
+  sim$massAttacksMap <- Reduce(modifyList, out)
+
+  annualAbundances <- lapply(sim$massAttacksMap, function(x) round(sum(x[], na.rm = TRUE), 0))
+  sim$massAttacksMap <- raster::stack(sim$massAttacksMap)
+
   setColors(sim$massAttacksMap) <- rep(list(brewer.pal(9, "YlOrRd")), nlayers(sim$massAttacksMap))
 
   sim$currentAttacks <- sim$massAttacksMap[[paste0("X", start(sim))]]
@@ -186,4 +185,126 @@ loadRasterStackTruncateYears <- function(fname, startTime) {
           " as these occur prior to start(sim)")
   allMaps <- dropLayer(allMaps, toDrop) ## drop layers that won't be used
   allMaps
+}
+
+prepInputsMPB_ABdata <- function(rasterToMatch, startYear, disaggregateFactor = 10, ...) {
+  fileInfo <- preProcess(..., archive = NA)
+  dirForExtract <- file.path(dirname(fileInfo$targetFilePath), rndstr(1))
+  out <- archive::archive_extract(fileInfo$targetFilePath,
+                                  dir = dirForExtract)
+
+  gdbName <- unique(dirname(out$path))[1]
+  origDir <- setwd(dirForExtract)
+  on.exit(setwd(origDir))
+  layerNames <- sf::st_layers(gdbName)
+  yrsAvail <- substr(layerNames$name, start = nchar("ab_0ufohn") + 1, stop = 13)
+  yrsAvail <- gsub(".$", "", yrsAvail)
+  layerNames$year <- yrsAvail
+
+  # Years are 2 digit -- so numbers above 70 are in 20th C; below 30 are 21st C.
+  yrs <- ifelse(as.numeric(yrsAvail) > 50, paste0(19, yrsAvail), paste0(20, yrsAvail))
+  yrs <- as.integer(yrs)
+  layerNames$yearNum <- yrs
+  yearsToDo <- startYear:(max(yrs))
+  lays <- as.data.table(sapply(layerNames, function(x) x))
+  rtmTemplate <- raster::raster(rasterToMatch)
+  rtmCRS <- st_crs(rasterToMatch)
+  if (disaggregateFactor > 1)
+    rasterToMatch <- raster(raster::disaggregate(rtmTemplate,
+                                                 fact = disaggregateFactor))
+
+  out <- list()
+  for (ytd in yearsToDo) {
+    yrsIn <- lays[yearNum == ytd]$name
+    names(yrsIn) <- yrsIn
+    if (NROW(yrsIn)) {
+      pointsAndPolys <- Cache(lapply, yrsIn, function(y) {
+        message(crayon::green(y))
+        co <- capture.output(mpbMap <- sf::st_read(gdbName, layer = y))
+        mpbMap <- st_transform(mpbMap, rtmCRS)
+        mpbMap <- fixErrors(mpbMap, useCache = FALSE)
+        if (NROW(mpbMap)) {
+          if (all(sf::st_is(mpbMap, "POINT"))) {
+            mpbRaster <- rtmTemplate
+            mpbMapSp <- sf::as_Spatial(mpbMap)
+            pixels <- cellFromXY(rtmTemplate, mpbMapSp)
+            whNA <- is.na(pixels)
+            colnam <- grep("num_trees", names(mpbMapSp), ignore.case = TRUE, value = TRUE)
+            mpbMapSpOnRTM <- mpbMapSp[!whNA,]
+            pixelsOnRTM <- cellFromXY(rtmTemplate, mpbMapSpOnRTM)
+            # pixels <- which(pixels)[!whNA]
+            dt1 <- data.table(num_trees = mpbMapSpOnRTM[[colnam]], pixel = pixelsOnRTM)
+            dt1 <- dt1[, list(num_trees = sum(num_trees)), by = "pixel" ]
+            message(crayon::green(paste0("  There are total ", sum(dt1$num_trees),
+                                         " attacked trees on map due to pixels")))
+            mpbRaster[dt1$pixel] <- dt1$num_trees
+          } else {
+            mpbMap <- st_cast(mpbMap, "MULTIPOLYGON")
+            rrr <- st_crop(mpbMap, rasterToMatch)
+            rrr$area_new <- sf::st_area(rrr)
+            totAbund <- round(sum(abundance(rrr$area_new/1e4, rrr$POLY_PERC, avgDensity = 1125)), 0)
+            message(crayon::green(paste0("  There are total ", totAbund,
+                                         " attacked trees on map due to polygons")))
+            # mpbRaster2 <- fasterize::fasterize(mpbMap, rtmTemplate, field = "POLY_PERC")
+            mpbRaster <- fasterize::fasterize(mpbMap, rasterToMatch, field = "POLY_PERC")
+            whNoNA <- which(!is.na(mpbRaster[]))
+            mpbRaster[whNoNA] <- abundance(areaPerUnit = (prod(res(mpbRaster)))/1e4, percentPerUnit = mpbRaster[whNoNA])
+            rasterizationDiff <- abs(sum(mpbRaster[], na.rm = T) - as.numeric(totAbund))/ length(whNoNA)
+            mess <- paste0("  Rasterization % deviation of total number of trees attacked: ", round(rasterizationDiff*100, 3))
+            if (rasterizationDiff > 0.001) {
+              message(crayon::red(mess))
+              warning(mess)
+            } else {
+              message(crayon::green(mess))
+            }
+
+            if (disaggregateFactor > 1) {
+
+              # data.table way
+              mpbRasterDT <- aggregateSum(mpbRaster, rtmTemplate)
+
+              # Raster package way is WAY TOO SLOW
+              # mpbRasterSmall <- raster::aggregate(mpbRaster, fact = disaggregateFactor, fun = sum)
+              mpbRaster <- mpbRasterDT
+            }
+          }
+        } else {
+          mpbRaster <- NULL
+        }
+        mpbRaster
+      })
+      pointsAndPolys <- pointsAndPolys[!sapply(pointsAndPolys, is.null)]
+      if (length(pointsAndPolys) > 1) {
+        pointsAndPolys <- raster::stack(pointsAndPolys)
+        pointsAndPolys <- calc(pointsAndPolys, sum, na.rm = TRUE)
+      } else {
+        pointsAndPolys <- pointsAndPolys[[1]]
+      }
+      pointsAndPolys[pointsAndPolys[]==0] <- NA
+      # aasSmall <- trim(aas)
+      setColors(pointsAndPolys) <- c("Reds")
+      out[[paste0("X",ytd)]] <- pointsAndPolys
+    }
+  }
+  out
+
+}
+
+abundance <- function(areaPerUnit, percentPerUnit, avgDensity = 1125) {
+  areaPerUnit*avgDensity*percentPerUnit/100
+}
+
+aggregateSum <- function(ras, newRas) {
+  whNonNA <- which(!is.na(ras[]))
+  rc2 <- rowColFromCell(ras, whNonNA)
+  if (!all(((res(newRas)/res(ras)) %% 1) == 0))
+    stop("The resolutions of the original raster and new raster are not integer multiples")
+  disaggregateFactor <- unique(res(newRas)/res(ras))
+  dt <- data.table(vals = ras[][whNonNA], ceiling(rc2 / disaggregateFactor))
+  dt2 <- dt[, list(vals = sum(vals)), by = c("row", "col")]
+  pixes <- cellFromRowCol(newRas, row = dt2$row, col = dt2$col)
+  newRasOut <- raster(newRas)
+  newRasOut[pixes] <- dt2$vals
+  newRasOut
+
 }
